c83d646fa78dd98cf7c3b95b77fa933e251bfc4e
LEONARDO DAVI DE SOUZA <57837644+LeonardoLDSZ@users.noreply.github.com>
2020-05-07 21:30:06 -0300

    Add files via upload

--- Corona.py
+++ Corona.py
@@ -1,6 +1,6 @@
-#Levantar informações do usuário a fim de verificar se este possui os sintomas do Corona Vírus(COVID-19)
-#sintomas: coriza + tosse + dor de garganta + febre + congestão nasal + diarreia + fadiga + cansaço + Dificuldade de respirar
-#fonte: https://www.bbc.com/portuguese/brasil-51946693
+# Levantar informações do usuário a fim de verificar se este possui os sintomas do Corona Vírus(COVID-19)
+# sintomas: coriza + tosse + dor de garganta + febre + congestão nasal + diarreia + fadiga + cansaço + Dificuldade de respirar
+# fonte: https://www.bbc.com/portuguese/brasil-51946693
 from time import sleep
 
 nome = input('Digite o seu nome:')
@@ -23,20 +23,18 @@ while opcao != 11:
     [ 11 ] Encerrar levantamento''')
     opcao = int(input('Qual seu sintoma?'))
     if opcao == 1 or opcao == 2 or opcao == 3 or opcao == 4 or opcao == 5 or opcao == 6 or opcao == 7 or opcao == 8 or opcao == 9:
-        print(f'{nome} {sobrenome}, {idade} anos de idade, não possui todos os sintomas do Corona Virús, contudo, deverá se dirigir ao posto de saúde mais próximo como precaução.')
+        print(
+            f'{nome} {sobrenome}, {idade} anos de idade, não possui todos os sintomas do Corona Virús, contudo, deverá ficar em isolamento como precaução.')
         break
     elif opcao == 10:
-        print(f'{nome} {sobrenome}, {idade} anos de idade, possui os sintomas do Corona Vírus e deverá se dirigir IMEDIATAMENTE ao posto de saúde mais próximo!') 
+        print(
+            f'{nome} {sobrenome}, {idade} anos de idade, possui os sintomas do Corona Vírus e deverá se dirigir IMEDIATAMENTE ao posto de saúde mais próximo!')
         break
     elif opcao == 11:
         print('Diagnóstico encerrado')
-    else: 
+    else:
         print('Opção inválida. Tente novamente')
     print('=-=' * 2)
     sleep(2)
 print('Fim do diagnóstico! Até a próxima!')
-                
-        
-
-
 
